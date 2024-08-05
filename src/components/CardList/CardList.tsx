404f9763049981cd6b22ae82a421f3eabbc48deb
Pilar <mariapilarcarranza@gmail.com>
2024-08-05 02:42:09 -0300

    mod

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,30 +1,20 @@
 import React from "react";
 import Card from "../Card/Card";
 import { getProductsDB } from "@/helpers/products.helper";
-import ICardProduct from "@/Interfaces/IProducts";
 import Link from "next/link";
 
 const CardList = async () => {
-  let products: ICardProduct[] = [];
-  try {
-    products = await getProductsDB(); // Espera que se resuelva esta petición porque es un componente del servidor, y luego resuelve lo que sigue
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
-
+  const products = await getProductsDB(); //espera que se resuelva esta petición porque es un componente del servidor, y luego resuelve lo que sigue
   return (
-    <div className="bg-white p-20 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-20 border border-gray-200 rounded-lg overflow-hidden">
-      {products.length > 0 ? (
-        products.map((product) => {
+    <div className="bg-[white] p-20 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-20 border border-gray-200 rounded-lg overflow-hidden">
+      {products &&
+        products?.map((product) => {
           return (
             <Link href={`/product/${product.id}`} key={product.id}>
               <Card key={product.id} {...product} />
             </Link>
           );
-        })
-      ) : (
-        <div>No products found</div>
-      )}
+        })}
     </div>
   );
 };
