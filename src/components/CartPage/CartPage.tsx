f9900f3bb3391b41d48d1f4881442b7942c8345d
Pilar <mariapilarcarranza@gmail.com>
2024-08-05 10:55:27 -0300

    S

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -44,12 +44,13 @@ const CartPage = () => {
     }
   }, []);
 
-  // // Redirigir al login si el usuario no está autenticado
-  // useEffect(() => {
-  //   if (!loading && (!userSession || !userSession.user?.name)) {
-  //     router.push("/login");
-  //   }
-  // }, [userSession, router, loading]);
+  // Redirigir al login si el usuario no está autenticado
+  useEffect(() => {
+    debugger;
+    if (!loading && (!userSession || !userSession.user?.name)) {
+      router.push("/login");
+    }
+  }, [userSession, router, loading]);
 
   const handleClick = async () => {
     if (cart.length > 0 && userSession?.token) {
